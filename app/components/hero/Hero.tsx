5dc798246a9de74d91f2d6dc64de1ee701f75007
Thiago Alves <thiago87gsa@gmail.com>
2026-09-09 22:34:52 -0300

    first commit

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div
+      className='min-h-screen bg-cover bg-center bg-no-repeat'
+      style={{ backgroundImage: "url('img/hero/Gemini_Generated_Image_8sc9ab8sc9ab8sc9.jpg')"}}
+    >
+      
+    </div>
+  );
+};
+export default Hero;
